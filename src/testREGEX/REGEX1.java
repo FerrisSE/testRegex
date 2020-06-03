a2e47ec1c21c483ab562932671feff3f84b97855
MohamedAbusharkh <abusham1@ferris.edu>
2020-06-03 16:29:45 -0400

    changed regex to dog123

--- src/testREGEX/REGEX1.java
+++ src/testREGEX/REGEX1.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 public class REGEX1{
 	public static void main(String args[]){
 		String regex= "(dog)*\\d";
-		String testedString= "dog7";
+		String testedString= "dog123";
 		boolean b = Pattern.matches(regex, testedString);
 		System.out.println(" the result is " + b);
 
