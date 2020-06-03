5ad617fe7dba780216223a11f92755427eda6136
MohamedAbusharkh <abusham1@ferris.edu>
2020-06-03 16:27:10 -0400

    added initial version of regex tester

---
+++ src/testREGEX/REGEX1.java
@@ -0,0 +1,16 @@
+package testREGEX;
+
+import java.util.regex.*;
+
+public class REGEX1{
+	public static void main(String args[]){
+		String regex= "(dog)*\\d";
+		String testedString= "dog7";
+		boolean b = Pattern.matches(regex, testedString);
+		System.out.println(" the result is " + b);
+
+	/** test documentation 
+	 * 
+	 */
+	}
+}
